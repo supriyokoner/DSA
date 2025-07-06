43c0dea48d6aaf707b73348e06ed8f736de48d96
Supriyo Koner <supriyokoner2005@gmail.com>
2025-07-07 01:23:54 +0530

    Update: Full solution

--- two_sum.java
+++ two_sum.java
@@ -1,3 +1,27 @@
-public class two_sum {
-    
+// Problem: https://leetcode.com/problems/two-sum/
+// Language: Java
+// Difficulty: Easy
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // HashMap to store number and its index
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // If complement exists, return indices
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            // Store current number with its index
+            map.put(nums[i], i);
+        }
+
+        // Return default value if no solution found (problem guarantees one solution)
+        return new int[]{-1, -1};
+    }
 }
